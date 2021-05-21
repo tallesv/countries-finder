a37f6bea0a1c200e8a038b6672595b6434b0bc55
Talles <talles17.a@hotmail.com>
2021-05-21 11:21:49 -0300

    feat: add country card and pagination mock

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React, { useState } from 'react';
+import Pagination from 'react-bootstrap/Pagination';
+import Card from 'react-bootstrap/esm/Card';
 import Header from './components/Header';
 import SearchInput from './components/SearchInput';
 import './styles/global.css';
 
 function App(): JSX.Element {
   const [textSearched, setTextSearched] = useState('');
+
+  const [currentPage, setCurrentPage] = useState(1);
+
   return (
     <>
       <Header />
@@ -12,7 +17,83 @@ function App(): JSX.Element {
       <main>
         <SearchInput onChangeInputValue={setTextSearched} />
 
-        {textSearched}
+        <Card>
+          <Card.Body>
+            <Card.Title className="title">
+              1 -
+              <img
+                src="https://restcountries.eu/data/afg.svg"
+                alt="country flag"
+                className="countryFlag"
+              />
+              Afghanistan
+            </Card.Title>
+            <Card.Text>Idiomas: Pashto, Uzbek and Turkmen</Card.Text>
+            <Card.Text>Moeda: Afghan afghani(EUR €) </Card.Text>
+          </Card.Body>
+        </Card>
+
+        <Card>
+          <Card.Body>
+            <Card.Title className="title">
+              1 -
+              <img
+                src="https://restcountries.eu/data/afg.svg"
+                alt="country flag"
+                className="countryFlag"
+              />
+              Afghanistan
+            </Card.Title>
+            <Card.Text>Idiomas: Pashto, Uzbek and Turkmen</Card.Text>
+            <Card.Text>Moeda: Afghan afghani(EUR €) </Card.Text>
+          </Card.Body>
+        </Card>
+
+        <Card>
+          <Card.Body>
+            <Card.Title className="title">
+              1 -
+              <img
+                src="https://restcountries.eu/data/afg.svg"
+                alt="country flag"
+                className="countryFlag"
+              />
+              Afghanistan
+            </Card.Title>
+            <Card.Text>Idiomas: Pashto, Uzbek and Turkmen</Card.Text>
+            <Card.Text>Moeda: Afghan afghani(EUR €) </Card.Text>
+          </Card.Body>
+        </Card>
+
+        <Card>
+          <Card.Body>
+            <Card.Title className="title">
+              1 -
+              <img
+                src="https://restcountries.eu/data/afg.svg"
+                alt="country flag"
+                className="countryFlag"
+              />
+              Afghanistan
+            </Card.Title>
+            <Card.Text>Idiomas: Pashto, Uzbek and Turkmen</Card.Text>
+            <Card.Text>Moeda: Afghan afghani(EUR €) </Card.Text>
+          </Card.Body>
+        </Card>
+
+        <Pagination>
+          <Pagination.First />
+          <Pagination.Prev />
+          <Pagination.Item active activeLabel="">
+            1
+          </Pagination.Item>
+          <Pagination.Item>2</Pagination.Item>
+          <Pagination.Item>3</Pagination.Item>
+          <Pagination.Item>4</Pagination.Item>
+          <Pagination.Item>5</Pagination.Item>
+          <Pagination.Next />
+          <Pagination.Last />
+        </Pagination>
       </main>
     </>
   );
